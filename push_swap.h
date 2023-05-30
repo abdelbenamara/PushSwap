ee5d58f169f0c057269c4dd706f5e11beb8c902f
abdelbenamara <abdelb.contact@gmail.com>
2023-05-30 23:54:54 +0100

    Updated push_swap

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 12:51:17 by abenamar          #+#    #+#             */
-/*   Updated: 2023/05/10 01:14:10 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/05/12 17:29:42 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,16 @@
 # include "limits.h"
 
 long	ft_atol(const char *nptr);
+int		ft_int(t_list *lst);
+uint8_t	ft_check(const char *str, long nb, t_list *lst);
+int		ft_extremum(t_list *lst, uint8_t max);
+uint8_t	ft_is_sorted(t_list *lst, uint8_t asc);
+
 void	ft_swap(t_list **lst);
-void	ft_push(t_list **lst, t_list *new);
+void	ft_push(t_list **lst1, t_list **lst2);
 void	ft_rotate(t_list **lst);
 void	ft_reverse_rotate(t_list **lst);
+
+void	ft_sort(t_list **lst_a);
 
 #endif
