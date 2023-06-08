c6b575a2f4a98a32d6e68b6cdef37fb715dfd294
abdelbenamara <abdelb.contact@gmail.com>
2023-06-08 17:02:38 +0100

    Updated ft_push

--- ft_push.c
+++ ft_push.c
@@ -6,17 +6,17 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 22:59:49 by abenamar          #+#    #+#             */
-/*   Updated: 2023/05/30 22:08:02 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/06/05 15:54:59 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_push(t_list **lst1, t_list **lst2)
+void	ft_push(t_list **lst_dst, t_list **lst_src)
 {
 	t_list	*item;
 
-	item = *lst2;
-	*lst2 = item->next;
-	ft_lstadd_front(lst1, item);
+	item = *lst_src;
+	*lst_src = item->next;
+	ft_lstadd_front(lst_dst, item);
 }
