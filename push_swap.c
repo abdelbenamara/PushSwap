86adcdce4291bb68f5710b14b58caf97eb6dca3a
abdelbenamara <abdelb.contact@gmail.com>
2023-06-08 17:02:50 +0100

    Updated push_swap

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 12:51:14 by abenamar          #+#    #+#             */
-/*   Updated: 2023/05/12 17:44:26 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/06/08 12:06:54 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,27 @@ int	main(int ac, char **av)
 {
 	t_list	*lst;
 	int		i;
+	int		*tab;
 	long	*nb;
 
 	if (ac < 2)
 		return (0);
 	lst = NULL;
 	i = 0;
+	tab = malloc((ac - 1) * sizeof(int));
+	if (!tab)
+		return (1);
 	while (++i < ac)
 	{
 		nb = malloc(sizeof(long));
 		if (!nb)
-			return (ft_lstclear(&lst, &free), 1);
+			return (ft_lstclear(&lst, &free), free(tab), 1);
 		*nb = ft_atol(av[i]);
 		if (!ft_check(av[i], *nb, lst) && ft_dprintf(STDERR_FILENO, "Error\n"))
-			return (free(nb), ft_lstclear(&lst, &free), 1);
+			return (free(nb), ft_lstclear(&lst, &free), free(tab), 1);
 		ft_lstadd_back(&lst, ft_lstnew(nb));
+		tab[i - 1] = (int) *nb;
 	}
-	ft_sort(&lst);
-	return (ft_lstclear(&lst, &free), 0);
+	ft_sort(ft_normalize(lst, tab, ac - 1));
+	return (ft_lstclear(&lst, &free), free(tab), 0);
 }
