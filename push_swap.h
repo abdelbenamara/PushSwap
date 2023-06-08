52982c9063cf89806339676449b531677514175d
abdelbenamara <abdelb.contact@gmail.com>
2023-06-08 17:02:22 +0100

    Updated push_swap

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 12:51:17 by abenamar          #+#    #+#             */
-/*   Updated: 2023/05/12 17:29:42 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/06/08 12:11:31 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@
 long	ft_atol(const char *nptr);
 int		ft_int(t_list *lst);
 uint8_t	ft_check(const char *str, long nb, t_list *lst);
-int		ft_extremum(t_list *lst, uint8_t max);
-uint8_t	ft_is_sorted(t_list *lst, uint8_t asc);
+t_list	*ft_normalize(t_list *lst, int *tab, int size);
 
 void	ft_swap(t_list **lst);
-void	ft_push(t_list **lst1, t_list **lst2);
+void	ft_push(t_list **lst_dst, t_list **lst_src);
 void	ft_rotate(t_list **lst);
 void	ft_reverse_rotate(t_list **lst);
 
-void	ft_sort(t_list **lst_a);
+void	ft_operate(char *op_a, t_list **lst_a, char *op_b, t_list **lst_b);
+void	ft_sort(t_list *lst_a);
 
 #endif
