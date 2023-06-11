c8118ca76b2d4d3a47ac1291bd7bd717da6c3124
abdelbenamara <abdelb.contact@gmail.com>
2023-06-11 22:51:34 +0100

    Updated ft_sort

--- ft_sort.c
+++ ft_sort.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 10:38:48 by abenamar          #+#    #+#             */
-/*   Updated: 2023/06/11 15:58:15 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/06/11 22:50:32 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,25 @@
 static void	ft_fill_b(char *op_a, t_list **lst_a, char *op_b, t_list **lst_b)
 {
 	const int	size = ft_lstsize(*lst_a) + ft_lstsize(*lst_b);
+	int			lst_a_size;
+	int			lst_b_size;
 	int			max;
 
+	lst_a_size = ft_lstsize(*lst_a);
+	lst_b_size = ft_lstsize(*lst_b);
 	max = 0;
-	while (ft_lstsize(*lst_a) > 3)
+	while (lst_a_size > 3)
 	{
-		if (ft_lstsize(*lst_b) == max)
-			max += ft_lstsize(*lst_a) / BRACKETS;
-		if (ft_lstsize(*lst_b) == max || max < 3 || max > size - 3)
+		if (lst_b_size == max)
+			max += lst_a_size / BRACKETS;
+		if (lst_b_size == max || max < 3 || max > size - 3)
 			max = size - 3;
 		ft_strlcpy(op_b, "", 1);
 		if (*lst_b && (*lst_b)->next && (ft_int(*lst_b) < max / 2
 				|| ft_int(*lst_b) < ft_int(ft_lstlast(*lst_b))))
 			ft_strlcpy(op_b, "rb", 3);
-		if (ft_int(*lst_a) <= max)
-			ft_strlcpy(op_a, "pb", 3);
+		if (ft_int(*lst_a) <= max && (++lst_b_size))
+			(ft_strlcpy(op_a, "pb", 3), --lst_a_size);
 		else
 			ft_strlcpy(op_a, "ra", 3);
 		ft_operate(op_a, lst_a, op_b, lst_b);
@@ -52,13 +56,14 @@ static void	ft_sort_a(t_list **lst_a, t_list **lst_b)
 	}
 }
 
-static void	ft_sort_b(int min, t_list **lst_a, char *op_b, t_list **lst_b)
+static int	ft_sort_b(int min, t_list **lst_a, char *op_b, t_list **lst_b)
 {
 	const int	size = ft_lstsize(*lst_a) + ft_lstsize(*lst_b);
+	int			lst_b_size;
 	int			pos;
 	t_list		*tmp;
 
-	ft_strlcpy(op_b, "", 1);
+	(ft_strlcpy(op_b, "", 1), lst_b_size = ft_lstsize(*lst_b));
 	pos = 0;
 	tmp = *lst_b;
 	while (tmp && (ft_int(tmp) <= min || (ft_int(tmp) > ft_int(*lst_a)
@@ -68,28 +73,31 @@ static void	ft_sort_b(int min, t_list **lst_a, char *op_b, t_list **lst_b)
 				&& ft_int(tmp) < ft_int(ft_lstlast(*lst_a)))) && (++pos))
 		tmp = tmp->next;
 	if (!tmp || ft_int(*lst_b) - 1 == ft_int(*lst_a))
-		return ;
-	else if (pos > ft_lstsize(*lst_b) / 2 || ((*lst_b)->next
+		return (lst_b_size);
+	else if (pos > lst_b_size / 2 || ((*lst_b)->next
 			&& ft_int(ft_lstlast(*lst_b)) + 1 == ft_int(*lst_a)))
 		ft_strlcpy(op_b, "rrb", 4);
 	else if (pos || ((*lst_b)->next
 			&& ft_int((*lst_b)->next) + 1 == ft_int(*lst_a)))
 		ft_strlcpy(op_b, "rb", 3);
 	else if (!pos && ft_int(tmp) < ft_int(*lst_a))
-		ft_strlcpy(op_b, "pa", 3);
+		return (ft_strlcpy(op_b, "pa", 3), lst_b_size - 1);
+	return (lst_b_size);
 }
 
 static void	ft_fill_a(char *op_a, t_list **lst_a, char *op_b, t_list **lst_b)
 {
 	const int	size = ft_lstsize(*lst_a) + ft_lstsize(*lst_b);
+	int			lst_b_size;
 	int			min;
 	t_list		*tmp;
 
-	min = ft_lstsize(*lst_b) / BRACKETS;
-	while (ft_lstsize(*lst_b) > min)
+	lst_b_size = ft_lstsize(*lst_b);
+	min = lst_b_size - size / BRACKETS;
+	while (lst_b_size > min && lst_b_size > 0)
 	{
 		ft_strlcpy(op_a, "", 1);
-		ft_sort_b(min, lst_a, op_b, lst_b);
+		lst_b_size = ft_sort_b(min, lst_a, op_b, lst_b);
 		if (ft_strncmp(op_b, "pa", 3))
 		{
 			tmp = *lst_a;
@@ -124,7 +132,9 @@ void	ft_sort(t_list *lst_a)
 	pos = 0;
 	while (ft_int(tmp) != 1 && (++pos))
 		tmp = tmp->next;
-	while (!ft_is_sorted(lst_a) && ((pos > ft_lstsize(lst_a) / 2
-				&& ft_strlcpy(op_a, "rra", 4)) || ft_strlcpy(op_a, "ra", 3)))
+	ft_strlcpy(op_a, "ra", 3);
+	if (pos > ft_lstsize(lst_a) / 2)
+		ft_strlcpy(op_a, "rra", 4);
+	while (!ft_is_sorted(lst_a))
 		ft_operate(op_a, &lst_a, "", &lst_b);
 }
