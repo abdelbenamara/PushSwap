be1d3a7c5b0782d8907503540977d2c1d4005551
abdelbenamara <abdelb.contact@gmail.com>
2023-06-16 00:42:19 +0100

    Updated push_swap

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 12:51:14 by abenamar          #+#    #+#             */
-/*   Updated: 2023/06/14 12:53:57 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/06/16 00:40:52 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,63 +61,27 @@ static void	ft_shellsort(int *tab, int size)
 	}
 }
 
-t_list	*ft_normalize(t_list *lst, int *tab)
-{
-	t_list	*new;
-	int		i;
-	int		*nb;
-
-	new = NULL;
-	while (lst)
-	{
-		i = 0;
-		while (tab[i] != ft_int(lst))
-			++i;
-		nb = malloc(sizeof(int));
-		if (!nb)
-			return (ft_lstclear(&new, &free), NULL);
-		*nb = i + 1;
-		ft_lstadd_back(&new, ft_lstnew(nb));
-		lst = lst->next;
-	}
-	return (new);
-}
-
-static int	ft_find_best_parts(t_list *lst, int *tab, int size)
+void	ft_normalize(int *tab, t_list **lst)
 {
 	t_list	*tmp;
-	int		parts;
-	int		best;
-	int		score;
-	int		test;
+	int		i;
 
-	parts = 2;
-	tmp = ft_normalize(lst, tab);
-	if (!tmp)
-		return (-1);
-	best = ft_sort(tmp, size, parts, 0);
-	test = parts;
-	while (++test, test < size / 100 + 3)
+	tmp = *lst;
+	while (tmp)
 	{
-		tmp = ft_normalize(lst, tab);
-		if (!tmp)
-			return (-1);
-		score = ft_sort(tmp, size, test, 0);
-		if (score < best)
-		{
-			best = score;
-			parts = test;
-		}
+		i = 0;
+		while (tab[i] != ft_int(tmp))
+			++i;
+		*((int *) tmp->content) = i + 1;
+		tmp = tmp->next;
 	}
-	return (parts);
 }
 
 int	main(int ac, char **av)
 {
-	int		*tab;
-	t_list	*lst;
-	int		parts;
-	t_list	*norm;
+	const int	splits = 3 + (ac - 1) / 250 - 1;
+	int			*tab;
+	t_list		*lst;
 
 	if (ac < 2)
 		return (0);
@@ -128,12 +92,8 @@ int	main(int ac, char **av)
 	if (!ft_parse_numbers(ac, av, tab, &lst))
 		return (1);
 	ft_shellsort(tab, ac - 1);
-	parts = -1;
-	while (parts == -1)
-		parts = ft_find_best_parts(lst, tab, ac - 1);
-	norm = ft_normalize(lst, tab);
-	if (!norm)
-		return (ft_lstclear(&lst, &free), free(tab), 1);
-	ft_sort(norm, ac - 1, parts, 1);
-	return (ft_lstclear(&lst, &free), free(tab), 0);
+	ft_normalize(tab, &lst);
+	while (!ft_sort(lst, ac - 1, splits))
+		;
+	return (free(tab), 0);
 }
