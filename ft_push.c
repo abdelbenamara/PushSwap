0fd92dafefac70cf9ee6cb7153ff7fa6865970bf
abdelbenamara <abdelb.contact@gmail.com>
2023-06-15 02:23:41 +0100

    Updated ft_push

--- ft_push.c
+++ ft_push.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 22:59:49 by abenamar          #+#    #+#             */
-/*   Updated: 2023/06/05 15:54:59 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/06/15 00:53:23 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_push(t_list **lst_dst, t_list **lst_src)
 {
 	t_list	*item;
 
+	if (!lst_src || !*lst_src)
+		return ;
 	item = *lst_src;
 	*lst_src = item->next;
 	ft_lstadd_front(lst_dst, item);
